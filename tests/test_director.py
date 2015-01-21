61c6c2687af9de1d5ccded1f1cb866a7638d8c31
PranavAngara19 <apranav19@gmail.com>
2015-01-20 19:09:50 -0800

    Add tests for data pre-processing in fetch_directions

--- tests/test_director.py
+++ tests/test_director.py
@@ -1,5 +1,6 @@
 from pydirections.director import Director
 from unittest import TestCase
+import re
 
 class TestModeValidity(TestCase):
 	def test_invalid_mode(self):
@@ -9,3 +10,24 @@ class TestModeValidity(TestCase):
 		default_error_message = "The mode: {0} is invalid"
 		invalid_mode = "flying"
 		self.assertFalse(Director.is_valid_mode(invalid_mode), msg=default_error_message.format(invalid_mode))
+
+class TestDirectionFetching(TestCase):
+	"""
+	    This class has test cases to cover the functionality of the fetch_directions function.
+	"""	
+	def test_invalid_or_missing_params(self):
+		"""
+			Tests if the required key-word args are missing or are invalid
+		"""
+		with self.assertRaises(ValueError):
+			Director.fetch_directions()
+
+		with self.assertRaises(ValueError):
+			Director.fetch_directions(origin="123 Fake Street Springfield, MA", dest="End")
+
+	def test_invalid_mode(self):
+		"""
+			Tests if an exception was raised if an invalid mode was provided
+		"""
+		with self.assertRaises(ValueError):
+			Director.fetch_directions(origin="San Francisco, CA", destination="Mountain View, CA", mode="flying")
