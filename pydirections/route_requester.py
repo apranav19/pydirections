207871f4f057d88f67bad0c371f880664dcee062
PranavAngara19 <apranav19@gmail.com>
2015-02-02 19:04:22 -0800

    Build custom route requester class

--- pydirections/route_requester.py
+++ pydirections/route_requester.py
@@ -5,11 +5,12 @@ ACCEPTABLE_MODES = set(["driving", "walking", "bicycling", "transit"])
 
 class DirectionsRequest(object):
 
-	def __init__(self, mode="driving", **kwargs):
-		self.mode = mode
+	def __init__(self, **kwargs):
+		self.mode = "driving"
 		self.origin = kwargs['origin']
 		self.destination = kwargs['destination']
 
+	
 	def set_api_key(self, key):
 		self.api_key = key
 		return self
