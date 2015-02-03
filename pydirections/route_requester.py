90be58f570af6b968721470c50092efdfa8c87f5
PranavAngara19 <apranav19@gmail.com>
2015-02-03 14:39:22 -0800

    Began refactoring tests and route_requester

--- pydirections/route_requester.py
+++ pydirections/route_requester.py
@@ -1,21 +1,35 @@
-
+from .exceptions import InvalidModeError, InvalidAPIKeyError
 ACCEPTABLE_MODES = set(["driving", "walking", "bicycling", "transit"])
 
 class DirectionsRequest(object):
-    """
-        This class holds all the necessary information required for a proposed route
-    """
-    def __init__(self, **kwargs):
-		self.mode = "driving"
-		self.origin = kwargs['origin']
-		self.destination = kwargs['destination']
+	"""
+		This class holds all the necessary information required for a proposed route
+	"""
+	def __init__(self, **kwargs):
+		"""
+			The constructor will set values for the required params i.e. origin & destination
+			Addtionally, it will set the default mode of transportation as driving
+		"""
+		self.__mode = "driving"
+		self.__origin = kwargs['origin']
+		self.__destination = kwargs['destination']
 
-    def set_api_key(self, key):
-        self.api_key = key
-        return self
+	@property
+	def api_key(self):
+		return self.__api_key
 
-    def set_mode(self, mode):
-    	if mode not in ACCEPTABLE_MODES:
-    		raise ValueError("Invalid mode")
-    	self.mode = mode
-    	return self
+	def set_api_key(self, api_key):
+		if type(api_key) != str:
+			raise InvalidAPIKeyError
+		self.__api_key = api_key
+		return self
+	
+	@property
+	def mode(self):
+	    return self.__mode
+
+	def set_mode(self, mode):
+		if mode not in ACCEPTABLE_MODES:
+			raise InvalidModeError(mode)
+		self.__mode = mode
+		return self
