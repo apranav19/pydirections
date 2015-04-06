ca0d59632da204d630b1ec02db53feaf98ea39f1
Pranav Angara <apranav19@gmail.com>
2015-04-06 14:42:07 -0700

    Refactor route responses

--- pydirections/route_responses.py
+++ pydirections/route_responses.py
@@ -1,21 +1,9 @@
-from future.utils import with_metaclass
-from abc import ABCMeta, abstractmethod
-
-class BaseResponse(with_metaclass(ABCMeta, object)):
+class DirectionsResponse(object):
 	"""
-		The Base Response class
-	"""
-	def __init__(self, stream):
-		self.__status = stream
-
-	@property
-	def status(self):
-		""" Returns the status code of the request made """
-		return self.__status
-
-class DirectionsResponse(BaseResponse):
-	"""
-		The Directions Response class represents a response which was obtained due to a successful request
+		The Directions Response class represents a response.
+		The two attributes include:
+		1. Status - Represents the status of the request
+		2. Routes - Represents an array of routes from origin to destination
 	"""
 	def __init__(self, status, routes):
 		self.__status = status
